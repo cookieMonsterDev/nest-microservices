80603510d2bfe490c827b747a83e1e0cfb2ae0de
cookieMonsterDev <mykhailo.toporkov@gmail.com>
2025-10-11 11:02:57 +0300

    feat: add sth

--- apps/posts/src/main.ts
+++ apps/posts/src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { PostsModule } from './posts.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(PostsModule);
-  await app.listen(process.env.port ?? 3000);
+  
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.KAFKA,
+    options: {
+      client: {
+        brokers: ['localhost:9092'],
+      },
+      consumer: {
+        groupId: 'posts-consumer',
+      },
+    },
+  });
+
+  await app.startAllMicroservices();
+  await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
--- apps/users/src/main.ts
+++ apps/users/src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.KAFKA,
+    options: {
+      client: {
+        brokers: ['localhost:9092'],
+      },
+      consumer: {
+        groupId: 'users-consumer',
+      },
+    },
+  });
+
+  await app.startAllMicroservices();
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
